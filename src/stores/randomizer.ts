86d8c3efc7ea4bd8ed34b0ed85680d227fdeb746
Saulo <saulo.m.ozorio@gmail.com>
2023-08-19 11:42:31 -0300

    feat: randomizer contract and interface usage

--- src/stores/randomizer.ts
+++ src/stores/randomizer.ts
@@ -1,21 +1,24 @@
-import { RandomizeService } from '@services/index'
-import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+import { RandomizeService } from '@services/index'
+
+import type { IUser } from '@interfaces/IRandomizer'
 
 export const useRandomizerStore = defineStore('randomizer', () => {
   // States
-  const user = ref({})
+  const user = ref({} as IUser)
   const isFetching = ref(false)
 
   // Actions
   /**
-   * Randomize a new user calling the API and returns a user object.
+   * Randomize a new user calling the API and define user state as an IUser object.
    */
   async function randomize() {
     isFetching.value = true
     try {
-      const { data } = await RandomizeService.randomize()
-      user.value = data
+      const response = await RandomizeService.randomize()
+      user.value = response
     } catch (error) {
       console.error(error)
     } finally {
